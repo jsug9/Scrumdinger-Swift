e8f74f6aca7f34bf511c5b94d5af3bde38a372b5
Augusto <jesus_augga_99@icloud.com>
2022-08-02 22:19:16 -0500

    Add enum for colors in theme model

--- Shared/Models/Theme.swift
+++ Shared/Models/Theme.swift
@@ -5,5 +5,34 @@
 //  Created by Augusto Galindo Alí on 2/08/22.
 //
 
-import Foundation
+import SwiftUI
 
+enum Theme: String {
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+
+    var accentColor: Color {
+        switch self {
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        }
+    }
+    
+    var mainColor: Color {
+        Color(rawValue)
+    }
+}
