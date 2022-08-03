9219ca9c5008533e7fd3444b8784457f4640d1fe
Augusto <jesus_augga_99@icloud.com>
2022-08-02 21:52:10 -0500

    Add circle for timer

--- Shared/MeetingView.swift
+++ Shared/MeetingView.swift
@@ -12,16 +12,20 @@ struct MeetingView: View {
         VStack {
             ProgressView(value: 5, total: 15)
             HStack {
-                VStack {
+                VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
+                        .font(.caption)
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
                 }
                 Spacer()
-                VStack {
+                VStack(alignment: .trailing) {
                     Text("Seconds Remaining")
+                        .font(.caption)
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
+            Circle()
+                .strokeBorder(lineWidth: 24)
         }
     }
 }
