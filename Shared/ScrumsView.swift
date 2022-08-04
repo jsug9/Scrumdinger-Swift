d7afd28d2da3b5ef8a96c41585234c1bfbcd0199
Augusto <jesus_augga_99@icloud.com>
2022-08-04 16:48:59 -0500

    Add details view

--- Shared/ScrumsView.swift
+++ Shared/ScrumsView.swift
@@ -13,7 +13,7 @@ struct ScrumsView: View {
     var body: some View {
         List {
             ForEach(scrums) { scrum in
-                NavigationLink(destination: Text(scrum.title)) {
+                NavigationLink(destination: DetailView(scrum: scrum)) {
                     CardView(scrum: scrum)
                 }
                 .listRowBackground(scrum.theme.mainColor)
@@ -34,5 +34,9 @@ struct ScrumsView_Previews: PreviewProvider {
         NavigationView {
             ScrumsView(scrums: DailyScrum.sampleData)
         }
+        NavigationView {
+            ScrumsView(scrums: DailyScrum.sampleData)
+        }
+        .preferredColorScheme(.dark)
     }
 }
