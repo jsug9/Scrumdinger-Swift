186badfc17cd50957552a444b63f457e6c79dddd
Augusto <jesus_augga_99@icloud.com>
2022-08-04 22:11:21 -0500

    Add detail edit view

---
+++ Shared/DetailEditView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger (iOS)
+//
+//  Created by Augusto Galindo Alí on 4/08/22.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
