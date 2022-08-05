9d62cec159a769cd8e3dc40ca01e84ad42d09c16
Augusto <jesus_augga_99@icloud.com>
2022-08-04 22:08:46 -0500

    Add navigation link from detailview to meetingview

--- Shared/DetailView.swift
+++ Shared/DetailView.swift
@@ -13,15 +13,19 @@ struct DetailView: View {
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
-                Label("Start Meeting", systemImage: "timer")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
+                
                 HStack {
                     Label("Length", systemImage: "clock")
                     Spacer()
                     Text("\(scrum.lengthInMinutes) minutes")
                 }
                 .accessibilityElement(children: .combine)
+                
                 HStack {
                     Label("Theme", systemImage: "paintpalette")
                     Spacer()
@@ -33,7 +37,14 @@ struct DetailView: View {
                 }
                 .accessibilityElement(children: .combine)
             }
+            
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
         }
+        .navigationTitle(scrum.title)
     }
 }
 
