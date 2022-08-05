550a4982fc2eeae0b97b665ea796dcf6d6cabe15
Augusto <jesus_augga_99@icloud.com>
2022-08-04 22:23:56 -0500

    Add meeting info and attendees to the form

--- Shared/DetailEditView.swift
+++ Shared/DetailEditView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct DetailEditView: View {
+    @State private var data = DailyScrum.Data()
+    @State private var newAttendeeName = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $data.title)
+                HStack {
+                    Slider(value: $data.lengthInMinutes, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(Int(data.lengthInMinutes)) minutes")
+                }
+            }
+            
+            Section(header: Text("Attendees")) {
+                ForEach(data.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    data.attendees.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            data.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
+                }
+            }
+        }
     }
 }
 
